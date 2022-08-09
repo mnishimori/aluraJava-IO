c247c89d0c6654bd616140e9303f8a6bcd11ef5b
mnishimori <mnishimori@yahoo.com.br>
2022-08-09 19:06:37 -0300

    refactor: writers

---
+++ src/TesteEscrita3.java
@@ -0,0 +1,19 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TesteEscrita3 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedWriter bw = new BufferedWriter(new FileWriter("lorem2.txt"));
+		
+		bw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+		bw.newLine();
+		bw.newLine();
+		bw.write("Teste OutputStream FileOutputStream Writer OutputStreamWriter BufferedWriter");
+		
+		bw.close();
+	}
+
+}
--- src/TesteLeituraEscrita.java
+++ src/TesteLeituraEscrita.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -34,6 +35,8 @@ public class TesteLeituraEscrita {
 		
 		br.close();
 		bw.close();
+		
+		FileWriter fw = new FileWriter("");
 	}
 
 }
