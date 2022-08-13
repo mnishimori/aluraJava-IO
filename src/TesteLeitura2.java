f543e7efa2dad08b56c0cb50475ba1e906d97ea7
mnishimori <mnishimori@yahoo.com.br>
2022-08-13 12:24:39 -0300

    feat: formatação de strings

--- src/TesteLeitura2.java
+++ src/TesteLeitura2.java
@@ -1,19 +1,33 @@
 import java.io.File;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeitura2 {
 
-	public static void main(String[] args) throws Exception {
-		
-		String linha = "";
-		Scanner sc = new Scanner(new File("contas.csv"));
-		
-		while (sc.hasNextLine()) {
-			linha = sc.nextLine();
-			System.out.println(linha);
-		}
-		
-		sc.close();
-	}
+      public static void main(String[] args) throws Exception {
 
+    	  Scanner scanner = new Scanner(new File("contas.csv"));
+          while(scanner.hasNextLine()) {
+                  String linha = scanner.nextLine();
+
+                  Scanner linhaScanner = new Scanner(linha);
+                  linhaScanner.useLocale(Locale.US);
+                  linhaScanner.useDelimiter(",");
+
+                  String tipo = linhaScanner.next();
+                  int agencia = linhaScanner.nextInt();
+                  int conta = linhaScanner.nextInt();
+                  String titular = linhaScanner.next();
+                  double valor = linhaScanner.nextDouble();
+                  
+                  System.out.println(String.format(new Locale("pt", "BR"), "%s %04d-%08d %20s %8.2f", tipo, agencia, conta, titular, valor));
+
+
+                  linhaScanner.close();
+
+//                  String[] valores = linha.split(",");
+//                  System.out.println(valores[1]);
+          }
+          scanner.close();
+      }
 }
